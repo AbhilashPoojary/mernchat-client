0987d28603fb8ba5e8180292b4b1ca809b3cb927
unknown <abhikanth5@gmail.com>
2022-04-12 23:12:22 +0530

    third commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export const loginUser = (user) => async (dispatch, getState) => {
   dispatch({ type: "USER_LOGIN_REQUEST" });
   try {
-    const response = await axios.post("/auth/login", user);
+    const response = await axios.post(
+      "https://mern-chat-messenger-post.herokuapp.com/api/auth/login",
+      user
+    );
     dispatch({ type: "USER_LOGIN_SUCCESS", payload: response.data });
     localStorage.setItem("currentUser", JSON.stringify(response.data));
     window.location.href = "/chat";
